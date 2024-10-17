0f20f6484132d78f1433c820274f3b0586c03b2b
Witchybun <embenham05@gmail.com>
2024-10-17 01:09:22 -0500

    increase version

--- Plugin.cs
+++ Plugin.cs
@@ -16,7 +16,7 @@ public class Plugin : BaseUnityPlugin
 {
     public const string PluginGUID = "com.Albrekka.FlipwitchAP";
     public const string PluginName = "FlipwitchAP";
-    public const string PluginVersion = "0.1.6";
+    public const string PluginVersion = "0.1.7";
     public const string ModDisplayInfo = $"{PluginName} v{PluginVersion}";
     private const string APDisplayInfo = $"Archipelago v{ArchipelagoClient.APVersion}";
     public static ArchipelagoClient ArchipelagoClient { get; private set; }
